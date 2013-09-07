0eeddbdf056e4c7179828b35c045ec6b02ebd7bc
Jerry Pennell <jwpenn@fullsail.edu>
2013-09-06 22:37:33 -0400

    Changed out layout and design
    added better icon and button for send

--- FroyoFoto/gen/com/jpennell/froyofoto/R.java
+++ FroyoFoto/gen/com/jpennell/froyofoto/R.java
@@ -21,7 +21,8 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btn_green=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080008;
@@ -80,5 +81,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int ButtonText=0x7f060002;
     }
 }

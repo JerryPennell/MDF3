a1653759ff52d01f4c152211dfcd142b69efdbd2
Jerry Pennell <jwpenn@fullsail.edu>
2013-09-24 22:45:34 -0400

    Added pintrest libs, parser and html layout
    Fixed issue with MainActivity crashing

--- PinToBuild/src/com/jpennell/pintobuild/MainActivity.java
+++ PinToBuild/src/com/jpennell/pintobuild/MainActivity.java
@@ -1,22 +1,149 @@
+/*
+ * project		PinToBuild
+ * 
+ * package		com.jpennell.pintobuild
+ * 
+ * author		Jerry Pennell
+ * 
+ * date			Sep 23, 2013
+ */
+
 package com.jpennell.pintobuild;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MainActivity.
+ */
 public class MainActivity extends Activity {
 
+	/** The spinner. */
+	Spinner spinner;
+	
+	/** The array boards. */
+	ArrayList<String> arrayBoards = new ArrayList<String>();
+	
+	/** The my handler. */
+	final Handler myHandler = new Handler();
+	
+	/** The spin item. */
+	String spinItem;
+
+
+	/**
+	 * Called when the activity is first created.
+	 *
+	 * @param savedInstanceState the saved instance state
+	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		new MyAsyncTask().execute();
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	/**
+ * The Class MyTask.
+ */
+private class MyAsyncTask extends AsyncTask<Void, Void, ArrayList<String>> {
 
+       //Set the pintrest URL
+		/** The Constant PINTREST_URL. */
+       protected static final String PINTREST_URL = "http://m.pinterest.com/";
+
+
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#doInBackground(Params[])
+		 */
+		@Override
+		protected ArrayList<String> doInBackground(Void... params) {
+
+			Document doc;
+			String linkText = ""; 
+
+			try {
+				doc = Jsoup.connect(PINTREST_URL + getIntent().getExtras().getString("USER")).get();
+				Elements links = doc.select("div.user_boards ul li a span");
+				arrayBoards.add("Select");
+				for (Element el : links) { 
+					linkText = el.text();
+
+					arrayBoards.add(linkText); 
+					Log.i("LINK: ",linkText);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return arrayBoards;     
+
+
+		} 
+
+
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+		 */
+		@Override
+		protected void onPostExecute(ArrayList<String> result) {        
+			
+			// Create an ArrayAdapter using the string array and a default spinner layout
+			Spinner spinner = (Spinner) findViewById(R.id.spinner); 
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, arrayBoards);
+			
+			// Specify the layout to use when the list of choices appears
+			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); 
+			
+			// Apply the adapter to my spinner
+			spinner.setAdapter(adapter);
+			Log.i("Spinner", "spinner populated");
+
+			spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+				@Override
+				public void onNothingSelected(AdapterView<?> parent) {
+				}
+
+				@Override
+				public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+					
+					if(pos > 0){
+						spinItem = parent.getItemAtPosition(pos).toString();
+						if (spinItem.equals("Select") || spinItem == "Select"){
+						} else {
+							String myParent = parent.getItemAtPosition(pos).toString().toLowerCase();	
+							myParent = myParent.replace(' ', '-');
+							String theURL = (PINTREST_URL +  getIntent().getExtras().getString("USER") +"/" + myParent);
+
+							WebView boardWebView = (WebView) findViewById(R.id.webview);
+							boardWebView.loadUrl(theURL);
+
+							Log.i("name", parent.getItemAtPosition(pos).toString());
+						}
+					}
+
+				}
+			});
+		}
+	}
 }
--- PinToBuild/src/com/jpennell/pintobuild/PinIt.java
+++ PinToBuild/src/com/jpennell/pintobuild/PinIt.java
@@ -1,7 +1,106 @@
+/*
+ * project		PinToBuild
+ * 
+ * package		com.jpennell.pintobuild
+ * 
+ * author		Jerry Pennell
+ * 
+ * date			Sep 24, 2013
+ */
+
 package com.jpennell.pintobuild;
 
-public class PinIt {
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PinUser.
+ */
+public class PinIt extends Activity {
 	
-	//Stub for class to pin something
+	/** The web view. */
+	WebView webView;
+	
+	final String DEFAULT_URL = "file:///android_asset/pinner.html";
+	
+	//updating
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_submit);
+
+		//FORCE PORTRAIT ORIENTATION
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+		//CREATE AND GET REFERENCE TO WEBVIEW AND ADD JAVASCRIPT COMPATIBILITY
+		WebView webView = (WebView) findViewById(R.id.webView1);
+		WebSettings theWebSettings = webView.getSettings();
+		theWebSettings.setAllowFileAccess(true);
+		theWebSettings.setJavaScriptEnabled(true);
+		webView.addJavascriptInterface(new JSInterface(this), "Android");	
+		webView.loadUrl(DEFAULT_URL);
+
+
+	}
+
+	//CREATE WEB INTERFACE TO HANDLE JAVASCRIPT FUNCTIONALITY
+	/**
+	 * The Class JSInterface.
+	 */
+	public class JSInterface{
+		
+		/** The _context. */
+		Context _context;
+
+		/**
+		 * Instantiates a new jS interface.
+		 *
+		 * @param c the c
+		 */
+		JSInterface(Context c){
+			_context = c;
+		}
+
+		/**
+		 * Send username over.
+		 *
+		 * @param user the user
+		 * @return true, if successful
+		 */
+		@JavascriptInterface
+		public boolean sendUsernameOver(String user){
+			Log.i("user", user);
+			Intent intent = new Intent(PinIt.this, MainActivity.class);
+			intent.putExtra("USER",  user); 
+	        startActivity(intent);
+
+			return true;
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }

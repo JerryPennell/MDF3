aed39f2542f235061de6173b9f1e41ee207f98c4
Jerry Pennell <jwpenn@fullsail.edu>
2013-09-26 22:19:12 -0400

    API appears to be dead

--- PinToBuild/src/com/jpennell/pintobuild/MainActivity.java
+++ PinToBuild/src/com/jpennell/pintobuild/MainActivity.java
@@ -20,7 +20,6 @@ import org.jsoup.select.Elements;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
